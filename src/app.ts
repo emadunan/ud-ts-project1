334c614606c4149a0017b1415dd89f652444d5c4
emadunan <emadunan@gmail.com>
2022-07-07 00:11:18 +0200

    Create interfaces for drag functionality

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,18 @@ enum projStatus {
     active, finished
 }
 
+// Drag Interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+}
+
 // Validator Function
 function validate(input: Validatable) {
     let isValid = true;
@@ -134,21 +146,42 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 // ProjItem Class
-class ProjItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
     private project: Project;
+
+    get persons() {
+        if (this.project.people === 1) {
+            return "1 person"
+        }
+        return `${this.project.people} persons`
+    }
     constructor(mainId: string, project: Project) {
         super("single-project", mainId, false, project.id);
         this.project = project;
 
+        this.configure()
         this.renderContent()
     }
 
+    @BindThis
+    dragStartHandler(event: DragEvent): void {
+        console.log(event);
+        
+    }
+
+    @BindThis
+    dragEndHandler(_event: DragEvent): void {
+        console.log("DragEnd!");
+        
+    }
+
     configure(): void {
-        throw new Error("Method not implemented.");
+        this.element.addEventListener("dragstart", this.dragStartHandler);
+        this.element.addEventListener("dragend", this.dragEndHandler);
     }
     renderContent(): void {
         this.element.querySelector("h2")!.textContent = this.project.title;
-        this.element.querySelector("h3")!.textContent = this.project.people.toString();
+        this.element.querySelector("h3")!.textContent = this.persons + " assigned";
         this.element.querySelector("p")!.textContent = this.project.desc;
     }
     
